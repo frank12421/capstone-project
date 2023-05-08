f1aa263a3bd4d2f97ecba4a1a81c7d09bf7189a3
Frank Böker <boeker.frank@gmail.com>
2023-05-08 05:58:10 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import ShowPlantList from "../components/ShowPlantList.js";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Grow Green</h1>
+      <h1>Hello from Grow Green - test</h1>
       <ShowPlantList />
     </div>
   );
