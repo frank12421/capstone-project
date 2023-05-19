b43923592da5516eb86d2cc99bbb40d005673399
Frank Böker <boeker.frank@gmail.com>
2023-05-19 15:27:48 +0200

    add sort for shodate on hp

--- components/ShowDates.js
+++ components/ShowDates.js
@@ -6,25 +6,26 @@ const StyeldTabel = styled.section`
   padding: 10px;
   display: grid;
   grid-template-columns: 25% 25% 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
   grid-gap: 3px px;
   background-color: bisque;
   margin-bottom: 20px;
 `;
 
 export default function ShowDates({ dates, places }) {
-  console.log("ShowDates-Dates", dates);
-  console.log("ShowDates-Places", places);
+  console.log("ShowDates-Dates", { dates });
+  // console.log("ShowDates-Places", places);
 
-  if (dates.length === 0) {
-    return <h5>ShowDates-Function-no Date</h5>;
-  }
+  const sortDates = [...dates].sort(
+    (a, b) => new Date(a.data.date) - new Date(b.data.date)
+  );
+  console.log("SortList", sortDates);
 
   function DefineWhen({ plantdate }) {
     const today = moment().format(`YYYY-MM-DD`);
     const weekofYear = moment().week();
-    console.log("Diese Woche", weekofYear);
-    console.log("Woche Plandate", moment(plantdate).week());
+    // console.log("Diese Woche", weekofYear);
+    // console.log("Woche Plandate", moment(plantdate).week());
     if (plantdate === today) {
       return <p>Heute</p>;
     } else if (moment(plantdate).week() === weekofYear) {
@@ -36,22 +37,20 @@ export default function ShowDates({ dates, places }) {
 
   return (
     <>
-      <h5>ShowDates-Function</h5>
       <StyeldTabel>
         <p key={"Wann"}>Wann</p>
         <p key={"Was"}>Was</p>
         <p key={"Wo"}>Wo</p>
-
-        {dates.map((date) => (
-          <>
-            {/* <p key={date.id}>{date.data.date}</p> */}
-            <DefineWhen key={date.id} plantdate={date.data.date} />
-            <p>{date.data.promptlist}</p>
-            {places.find((place) => place.id === date.location) && (
-              <p>{places[date.location - 1].name}</p>
-            )}
-          </>
-        ))}
+        {dates.length !== 0 &&
+          sortDates.map((date) => (
+            <>
+              <DefineWhen key={date.id} plantdate={date.data.date} />
+              <p>{date.data.promptlist}</p>
+              {places.find((place) => place.id === date.location) && (
+                <p>{places[date.location - 1].name}</p>
+              )}
+            </>
+          ))}
       </StyeldTabel>
     </>
   );
