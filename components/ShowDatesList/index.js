0a64195297b68b399f94d41dd9e3ecae736de24e
Frank Böker <boeker.frank@gmail.com>
2023-05-23 08:36:59 +0200

    component tidy and structured

--- components/ShowDatesList/index.js
+++ components/ShowDatesList/index.js
@@ -1,62 +1,11 @@
-import styled from "styled-components";
 import moment from "moment";
 import { useState } from "react";
-
-const StyeldDatesList = styled.section`
-  width: 100%;
-  margin: 0;
-  max-height: 155px;
-  display: flex;
-  flex-direction: column;
-  background-color: lightgrey;
-  margin-bottom: 20px;
-  overflow-y: auto;
-`;
-
-const StyledDatesListRow = styled.ul`
-  margin: 0;
-  display: grid;
-  grid-template-columns: 25% 30% 1fr;
-  grid-auto-rows: 1.4rem;
-  align-items: center;
-  justify-items: start;
-  list-style: none;
-  padding-left: 0.5rem;
-
-  position: ${(props) => (props.$head ? "sticky" : "static")};
-  top: ${(props) => (props.$head ? "0px" : "20px")};
-  background-color: ${(props) => (props.$head ? "darkgray" : "")};
-  color: ${(props) => (props.$head ? "white" : "black")};
-  cursor: ${(props) => (props.$hover ? "pointer" : "")};
-  &:hover {
-    font-weight: ${(props) => (props.$hover ? 900 : 300)};
-  }
-`;
-
-const StyledDatesDetailCard = styled.article`
-  position: relative;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-auto-rows: 1.4rem;
-  align-items: center;
-  justify-items: start;
-  padding: 10px 10px 10px 20px;
-`;
-
-const StyledDatesDetailCardButton = styled.button`
-  position: absolute;
-  top: 0;
-  right: 0;
-  background-color: darkblue;
-  color: lightgray;
-  text-decoration: none;
-  border-style: none;
-  font-size: 1.5rem;
-  cursor: pointer;
-  &:hover {
-    font-weight: 900;
-  }
-`;
+import {
+  StyeldDatesList,
+  StyledDatesDetailCard,
+  StyledDatesDetailCardButton,
+  StyledDatesListRow,
+} from "./styled";
 
 export default function ShowDateList({ dates, places }) {
   const sortDates = [...dates]
@@ -123,7 +72,7 @@ export default function ShowDateList({ dates, places }) {
       return (
         <StyledDatesListRow>
           <p>Heute nix</p>
-          <p>Chillen mit Grow Green</p>
+          <p>Chillen</p>
           <p>Wo immer du magst</p>
         </StyledDatesListRow>
       );
