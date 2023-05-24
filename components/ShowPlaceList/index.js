834a7b6baca51cc85b838baadd01e5059169d3e6
Frank Böker <boeker.frank@gmail.com>
2023-05-24 12:00:18 +0200

    made a component from site-placelist

---
+++ components/ShowPlaceList/index.js
@@ -0,0 +1,82 @@
+import { StyledLink } from "@/components/StyledButton";
+import {
+  StyledCard,
+  StyledCardList,
+  StyledContentContainer,
+  StyledCardListItem,
+  StyledCardFooter,
+} from "@/components/Card";
+import { StyledHeader } from "@/components/Header";
+import { StyledMain } from "@/components/Main";
+import StyledNavigation from "@/components/Navigation";
+import Image from "next/image";
+
+export default function ShowPlaceList({ places }) {
+  return (
+    <>
+      <StyledHeader>
+        <h1>Alle Standorte - New Grow Green</h1>
+        <StyledNavigation navigationlink="/" color="darkgreen">
+          zurück
+        </StyledNavigation>
+      </StyledHeader>
+      <StyledMain>
+        {places.map((place) => {
+          const imgSrc =
+            place.location !== ""
+              ? `/pictures/Place-${place.location}.png`
+              : `/pictures/Place-People.png`;
+
+          return (
+            <StyledCard border={"darkred"} key={place.id}>
+              <h2>{place.name}</h2>
+              <StyledContentContainer>
+                <Image
+                  src={imgSrc}
+                  alt="Pflanze"
+                  width="50"
+                  height="100"
+                  style={{ objectFit: "contain" }}
+                />
+                <StyledCardList>
+                  <StyledCardListItem>
+                    Kapazität: {place.used} / {place.capacity}
+                  </StyledCardListItem>
+                  <StyledCardListItem>
+                    Licht: {place.lightratio}
+                  </StyledCardListItem>
+                  <StyledCardListItem>
+                    Standort: {place.location}
+                  </StyledCardListItem>
+                  <StyledCardListItem>
+                    Regenschutz: {place.rainprotection}
+                  </StyledCardListItem>
+                </StyledCardList>
+              </StyledContentContainer>
+              <StyledCardFooter>
+                <StyledLink
+                  href={{
+                    pathname: `/lists/addplanttoplace`,
+                    query: { id: place.id },
+                  }}
+                  color={"green"}
+                >
+                  Pflanzen bearbeiten
+                </StyledLink>
+                <StyledLink
+                  href={{
+                    pathname: `/lists/showdateform`,
+                    query: { id: place.id },
+                  }}
+                  color={"darkred"}
+                >
+                  Termin anlegen
+                </StyledLink>
+              </StyledCardFooter>
+            </StyledCard>
+          );
+        })}
+      </StyledMain>
+    </>
+  );
+}
