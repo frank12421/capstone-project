fc45b980da75dddd89f9472c5631a1154a9a6152
Frank Boeker <boeker.frank@gmail.com>
2023-07-04 12:59:50 +0200

    add SubCards delete and edit to dates

--- components/Dates/DateCardLong/index.js
+++ components/Dates/DateCardLong/index.js
@@ -13,6 +13,9 @@ import {
 import { useState } from "react";
 import { mutate } from "swr";
 import { router } from "next/router";
+import { StyledSubCardContainer } from "@/components/Styling/StyledSubCard";
+import { SubCardDelete } from "@/components/SubCards/SubCardDelete";
+import { SubCardEdit } from "@/components/SubCards/SubCardEdit";
 
 export default function DateCardLong({ oneDate }) {
   const date = oneDate.data;
@@ -68,28 +71,18 @@ export default function DateCardLong({ oneDate }) {
             <StyledIconSettings />
           </StyledCircleButton>
         </CardInfoLinkButton>
+        {toggleSettings && (
+     <StyledSubCardContainer>
+     <SubCardDelete onClick={() => handleDeleteClick(true, oneDate._id)}
+     color="globalDateBackgroundColor" backgroundcolor="globalNavigationBackgroundColor">Termin löschen</SubCardDelete>
+     
+     <SubCardEdit onClick={() => handleEditClick(oneDate._id)}
+     color="globalNavigationIconColor" backgroundcolor="globalNavigationPlantColor">Termin bearbeiten</SubCardEdit>
+     </StyledSubCardContainer>
+     
+          )}
       </CardContainer>
-      {toggleSettings && (
-        <CardContainer
-          backgroundcolor="globalDateBackgroundColor"
-          //ref={confirmationRef}
-        >
-          <h3>Termin löschen?</h3>
-          <StyledCircleButton
-            type="button"
-            onClick={() => handleDeleteClick(true, oneDate._id)}
-          >
-            <StyledIconTrash color="globalNavigationPlantColor" />
-          </StyledCircleButton>
-          <h3>Termin bearbeiten:</h3>
-          <StyledCircleButton
-            type="button"
-            onClick={() => handleEditClick(oneDate._id)}
-          >
-            <StyledIconEdit color="globalNavigationIconColor" />
-          </StyledCircleButton>
-        </CardContainer>
-      )}
+     
     </>
   );
 }
